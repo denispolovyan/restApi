7e592177998eba2d7934ce700c0f45c76397a5ac
denispolovyan <denispolovyan@gmail.com>
2023-10-13 17:01:08 +0300

    feature: created GET endpoints /users /users/:id /users?name=name

---
+++ constants.js
@@ -0,0 +1,8 @@
+export const db = {
+  users: [
+    { id: 1, name: "Alex", age: 22, balance: 400 },
+    { id: 2, name: "Den", age: 27, balance: 4800 },
+    { id: 3, name: "Max", age: 14, balance: 100 },
+    { id: 4, name: "Ostin", age: 32, balance: 2200 },
+  ],
+};
--- index.js
+++ index.js
@@ -1,14 +1,56 @@
 import express from "express";
 
+import { db } from "./constants.js";
+
+let users = db.users;
+
 const PORT = 5000;
 
 const app = express();
-
 app.use(express.json());
 
+//   ---- // MAIN
+
 app.get("/", (req, res) => {
-  res.send({status: "main"});
+  //   res.json({status: "main"});
+  res.json("It is main page");
 });
+
+//   ---- // USERS
+
+app.get("/users", (req, res) => {
+  let filteredUsers = [];
+
+  if (req.query.name) {
+    users.forEach((user) => {
+      if (user.name == req.query.name) {
+        filteredUsers.push(user);
+      }
+    });
+  } else {
+    filteredUsers = users;
+  }
+
+  res.json(filteredUsers);
+});
+
+app.get("/users/:id", (req, res) => {
+  const user = users.find((user) => user.id == req.params.id);
+
+  if (!user) {
+    res.sendStatus(404);
+    return;
+  }
+  res.json(user);
+});
+
+app.get("/users/balance", (req, res) => {
+  const balance = users.reduce((acc, user) => acc + user.balance);
+
+  console.log(balance);
+});
+
+// ======>>> LISTEN
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
